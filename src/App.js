86543a7beff697a4d071bd2a18e80485144a1de6
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-22 19:01:32 +0200

    ✨Add favourites feature

--- src/App.js
+++ src/App.js
@@ -27,10 +27,12 @@ function App() {
   });
   async function loadCharacters(name, page) {
     const characters = await getCharacters(name, page);
+    const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
     const characterElements = characters.results.map((character) =>
       Character({
         name: character.name,
         imgSrc: character.image,
+        isFavorite: favorites.includes(character.name),
       })
     );
     characterContainer.append(...characterElements);
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,9 @@
 import { createElement } from "../utils/elements";
+import starActiveSrc from "../assets/staractive.svg";
+import starInactiveSrc from "../assets/starinactive.svg";
 import "./character.css";
 
-export function Character({ name, imgSrc }) {
+export function Character({ name, imgSrc, isFavourite }) {
   const avatar = createElement("img", {
     className: "character__img",
     src: imgSrc,
@@ -13,9 +15,48 @@ export function Character({ name, imgSrc }) {
     innerText: name,
   });
 
+  const favouriteImg = createElement("img", {
+    className: "favourite__img",
+    src: isFavourite ? starActiveSrc : starInactiveSrc,
+  });
+
+  const favouriteBtn = createElement("button", {
+    className: "favourite__btn",
+    onclick: () => {
+      // onclick: variable with current favorites -> verwandeln in JSON
+      //get favorites from localStorage ||or empty, if there are no favs yet
+      let currentFavorites = JSON.parse(
+        localStorage.getItem("favorites") || "[]"
+      );
+
+      //condition: currentFavorites already contain selected name,
+      const isFavorite = currentFavorites.includes(name);
+      //if this is true, then take the currentFavourites and filter them, by removing the name of the selected favourite
+      if (isFavorite) {
+        currentFavorites = currentFavorites.filter(
+          //!== strict inequality operator - favourite can not be equal to name
+          (favorite) => favorite !== name
+        );
+
+        //if selected name is not yet a favourite, then push name to array
+      } else {
+        currentFavorites.push(name);
+      }
+      localStorage.setItem("favourites", JSON.stringify(currentFavorites));
+      favouriteImg.src = !isFavourite ? starActiveSrc : starInactiveSrc;
+    },
+
+    children: [favouriteImg],
+  });
+
+  const header = createElement("div", {
+    className: "character__header",
+    children: [title, favouriteBtn],
+  });
+
   const container = createElement("article", {
     className: "character",
-    children: [title, avatar],
+    children: [header, avatar],
   });
 
   return container;
